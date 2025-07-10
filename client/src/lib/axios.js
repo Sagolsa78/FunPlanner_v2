c0e4b97751b7b9ba991c8005a07fa1ba3477e655
OmXDev <omgupta.tech@gmail.com>
2025-07-10 09:55:37 +0530

    solving issues of todos

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -4,3 +4,14 @@ export const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
